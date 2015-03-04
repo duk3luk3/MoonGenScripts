e31b4e68d08128d7609adea97bd06e11cb73a7ba
Lukas Erlacher <l.erlacher@gmail.com>
2015-03-04 22:05:12 +0100

    fix

--- l3-load-latency.lua
+++ l3-load-latency.lua
@@ -40,6 +40,7 @@ function loadSlave(port, queue, size, numFlows)
 		ts.fillPacket(buf, 1234, size)
 		local data = ffi.cast("uint8_t*", buf.pkt.data)
 		data[43] = 0x00 -- PTP version, set to 0 to disable timestamping for load packets
+		local pkt = buf:getUdpPacket()
 		pkt.eth.src:setString("90:e2:ba:2c:cb:02") -- klaipeda eth-test1 MAC
 		pkt.eth.dst:setString("90:e2:ba:35:b5:81") -- tartu eth-test1 MAC
 		pkt.ip.dst:set(0xc0a80102) -- 192.168.1.2
